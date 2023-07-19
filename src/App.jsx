b8e83a8f2d2c008c050f85c620bd663c3d71a9cc
Rivadeneiea <joserivadeneira538@gmail.com>
2023-07-19 20:03:40 -0300

    mapeamos item unicos

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
       <Container className=" my-5 mainPage ">
         <h1 className="display-5 text-center text-light">Lista de Tareas</h1>
         <FormularioTarea></FormularioTarea>
+        
       </Container>
 
       <footer className="text-center ">
--- src/components/ListaTareas.jsx
+++ src/components/ListaTareas.jsx
@@ -4,11 +4,11 @@ import ItemTarea from "./ItemTarea";
 const ListaTareas = ({ propslistaTareas }) => {
   return (
     <ListGroup>
-      {
-      propslistaTareas.map(()=> <ItemTarea></ItemTarea>
-  )} 
-  </ListGroup>)}
-  
-
+      {propslistaTareas.map((tarea, index) => (
+        <ItemTarea key={index}></ItemTarea>
+      ))}
+    </ListGroup>
+  );
+};
 
 export default ListaTareas;
